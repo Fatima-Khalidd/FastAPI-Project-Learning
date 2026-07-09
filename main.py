149359bbd916ef340a2ecf10f5ed400665efbe9c
Fatima-Khalidd <fatimakhalidddd0@gmail.com>
2026-07-09 19:04:20 +0500

    pytest-api-testing

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from config import settings
+from fastapi import FastAPI, HTTPException
+
 app = FastAPI()
-
-
-# all allowed origin (front-end)
-origin =settings.ORIGINS # using 'ORIGINS' value from .env file
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origin, # frontend 
-    allow_credentials=True,
-    allow_methods=["*"], # '*' means to allow for all CURD Operations
-    allow_headers=["*"] 
-)
-
-@app.get("/")  # this api (in backend) will bw accessed by frontend
+@app.get("/")
 def home():
-    return {
-        "message":"CORS ENABLED API"
+    return{
+        "message":"Hello Fatima"
+    }
+@app.get("/add")
+def add(a:int,b:int):
+    return{
+        "result":a+b
     }
