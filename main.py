27dd3ed8905581747b5f2a3fc7eebdd9af575cd4
Fatima-Khalidd <fatimakhalidddd0@gmail.com>
2026-07-10 01:38:29 +0500

    add cache api

--- main.py
+++ main.py
@@ -1,65 +1,40 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from fastapi import FastAPI,HTTPException
+import requests
+from bs4 import BeautifulSoup
+import time
 
-app = FastAPI()
+app=FastAPI()
 
-DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-sessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
+# cache storage
 
-class Todo(Base):
-    __tablename__ = "todos"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    completed = Column(String)
+cache_data=[]
+last_fetch=0
 
-Base.metadata.create_all(bind=engine)
 
-def get_db():
-    db = sessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+@app.get("/news")
+def get_news():
+    global cache_data,last_fetch
 
-@app.post("/todos")
-def create_todo(title: str, db: Session = Depends(get_db)):
-    todo = Todo(title=title, completed="False")
-    db.add(todo)
-    db.commit()
-    db.refresh(todo)
-    return {"message": "Todo Created", "data": todo}
+    start=time.time()
+    if time.time() - last_fetch>60:
+        print("Fetching fresh data")
+        url="https://news.ycombinator.com/"  # website from where wewill crawl/get/fetch data
 
-@app.get("/todos")
-def get_todos(db: Session = Depends(get_db)):
-    todos = db.query(Todo).all()
-    return {"Total Data": len(todos), "message": "Todos Fetched", "data": todos}
+        response=requests.get(url) # fetching data and saving in response variable
+        soup=BeautifulSoup(response.text,"html.parser")
+        cache_data=[
+            item.text for item in soup.find_all("span",class_="titleline")
+        ]
 
-@app.get("/todos/{todo_id}")
-def get_todo_by_id(todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
-    if not todo:
-        raise HTTPException(status_code=404, detail=f"Todo with id {todo_id} not found")
-    return todo
+        last_fetch=time.time()
+    else:
+        print("Using cached data")
+    
+    end=time.time()
+    time_taken=round(end-start,4)
+    print("Time Taken: ",time_taken)
 
-@app.put("/todos/{todo_id}")
-def update_todo(todo_id: int, title: str, completed: str, db: Session = Depends(get_db)):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
-    if not todo:
-        raise HTTPException(status_code=404, detail=f"Todo with id {todo_id} not found")
-    todo.title = title
-    todo.completed = completed
-    db.commit()
-    db.refresh(todo)
-    return {"message": "Todo Updated", "data": todo}
-
-@app.delete("/todos/{todo_id}")
-def delete_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo = db.query(Todo).filter(Todo.id == todo_id).first()
-    if not todo:
-        raise HTTPException(status_code=404, detail=f"Todo with id {todo_id} not found")
-    db.delete(todo)
-    db.commit()
-    return {"message": "Todo Deleted", "Todo Deleted": todo}
+    return {
+        "time_taken":time_taken,
+        "data":cache_data[:5]
+    }
