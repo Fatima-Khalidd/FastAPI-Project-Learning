2bcb0fe4128e114e2e00d9b68e0e257a9dc857ca
Fatima-Khalidd <fatimakhalidddd0@gmail.com>
2026-07-09 15:22:29 +0500

    CORS-handling-FE-BE-connection

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
-import shutil, os
+from fastapi.middleware import CORSMiddleware
 
 app = FastAPI()
 
-UPLOAD_DIR = "uploads"
-if not os.path.exists(UPLOAD_DIR):
-    os.makedirs(UPLOAD_DIR)
+# all allowed origin (front-end)
+origin =[
+    "http://localhost:5173" # front end url
+]
 
-app.mount("/uploads", StaticFiles(directory=UPLOAD_DIR), name="uploads")
+app.add_middleware(
+    
+    allow_origins=origin, # frontend 
+    allow_credentials=True,
+    allow_methods=["*"], # '*' means to allow for all CURD Operations
+    allow_headers=["*"] 
+)
 
-@app.post("/upload")
-def upload_file(file: UploadFile = File(...)):
-    filename = file.filename
-    file_path = os.path.join(UPLOAD_DIR, filename)
-    if not filename:
-        raise HTTPException(status_code=400, detail="No file selected for upload")
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    return {"message": "File uploaded successfully", "filename": filename}
-
-@app.get("/download/{filename}")
-def download_file(filename: str):
-    file_path = os.path.join(UPLOAD_DIR, filename)
-    if not os.path.exists(file_path):
-        raise HTTPException(status_code=404, detail="File not found")
-    return FileResponse(path=file_path, filename=filename)
-
-@app.get("/")
+@app.get("/")  # this api (in backend) will bw accessed by frontend
 def home():
-    return {"message": "File Upload API Running"}
+    return {
+        "message":"CORS ENABLED API"
+    }
